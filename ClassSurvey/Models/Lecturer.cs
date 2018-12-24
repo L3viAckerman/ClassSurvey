1e99d4f4414c3f568c6bdc4769d120be3201394a
LinhLPV <lephamvanlinh@gmail.com>
2018-12-24 22:09:35 +0700

    ???

---
+++ ClassSurvey/Models/Lecturer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassSurvey1.Models
+{
+    public partial class Lecturer
+    {
+        public Lecturer()
+        {
+            Classes = new HashSet<Class>();
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public int? Role { get; set; }
+        public string LecturerCode { get; set; }
+        public string Vnumail { get; set; }
+        public string Phone { get; set; }
+
+        public User IdNavigation { get; set; }
+        public ICollection<Class> Classes { get; set; }
+    }
+}
