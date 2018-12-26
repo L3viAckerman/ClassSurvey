5df713648e7e9356005ce836bef90a9cccae31f3
LinhLPV <lephamvanlinh@gmail.com>
2018-12-26 11:57:27 +0700

    fix bugs and change models from DB

---
+++ ClassSurvey/Modules/MUsers/UserEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassSurvey.Modules.MUsers
+{
+    public class UserEntity
+    {
+    }
+}
